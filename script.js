286dea6c0c7aec81273f1e8d3d49e858ad94b34b
itzSeyditz <113749609+iazula47@users.noreply.github.com>
2025-03-24 16:36:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const photos = [
       photoElement.src = photos[index];
       // Set size each time (not necessary if already set)
       photoElement.style.width = '600px';
-      photoElement.style.height = '690px';
+      photoElement.style.height = '700px';
     }, 300);
   
     setTimeout(() => {
